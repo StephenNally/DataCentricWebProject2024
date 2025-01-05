2eb389e4b5744a27b5b7704c433486b456d86195
Stephen Nally <g00418614@atu.ie>
2025-01-05 19:22:18 +0000

    Add grades page with data from MySQL

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ var express = require('express');
 var app = express();
 var studentsRoutes = require('./routes/students');
 var lecturersRoutes = require('./routes/lecturers');
+var gradesRoutes = require('./routes/grades');
 
 app.set('view engine', 'ejs');
 
 // Routes
 app.use('/students', studentsRoutes);
 app.use('/lecturers', lecturersRoutes);
+app.use('/grades', gradesRoutes);
 
 app.listen(3004, () => {
   console.log('Server running on port 3004');
---
+++ routes/grades.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../MySqlDao');
+
+// GET Grades Page
+router.get('/', async (req, res) => {
+  try {
+    const [grades] = await db.query(`
+      SELECT s.name AS studentName, m.name AS moduleName, g.grade
+      FROM grade g
+      JOIN student s ON g.sid = s.sid
+      JOIN module m ON g.mid = m.mid
+      ORDER BY s.name, g.grade;
+    `);
+    res.render('grades', { grades });
+  } catch (err) {
+    console.error('Error fetching grades:', err);
+    res.status(500).send('Error fetching grades');
+  }
+});
+
+module.exports = router;
